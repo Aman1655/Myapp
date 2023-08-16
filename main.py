43b8ae14f77d0aa1165e90694ea2c58a75d8f58b
Aman Jha <93602424+Aman1655@users.noreply.github.com>
2023-08-16 19:19:12 +0530

    initial

---
+++ main.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        return Label(text='Hello, Aman \n looking forward in this journey as your PA \n well my name is MC \n at your service')
+
+if __name__ == '__main__':
+    MyApp().run()
